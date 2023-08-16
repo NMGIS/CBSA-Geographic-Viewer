93f7531f7fa720c3f2a20a3c13a7befebfedd675
Nevin McIntyre <NevinMcIntyreGIS@gmail.com>
2023-08-16 15:03:23 -0700

    Styling of layers

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,29 +3,12 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoibm1naXMiLCJhIjoiY2xsZDFlZDcwMDVweDNlbDAydG81M
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: 'mapbox://styles/mapbox/satellite-streets-v11', // style URL
+    style: 'mapbox://styles/nmgis/clle6s78z00gu01rd16or251q', // style URL
     center: [-98, 39], // starting position [lng, lat]
     zoom: 5 // starting zoom
   });
 
 map.on('load', function() {
-    map.addSource('Place', {
-        'type': 'vector',
-        'url': 'mapbox://nmgis.59eos659'
-    });
-
-    map.addLayer({
-        'id': 'Place_Layer',
-        'type': 'fill', // Can be 'fill', 'line', 'circle', 'symbol', etc. based on your data
-        'source': 'Place',
-        'source-layer': 'Incorporated_Place_Related-1qqx0j', // This is usually the name of your tileset
-        'layout': {},
-        'paint': {
-            'fill-color': '#FF5733',  // Bright color
-            'fill-opacity': 0.8,     // High opacity
-            'fill-outline-color': '#8C1D00'  // Thin dark outline
-        }
-    });
     
     map.addSource('Metro', {
         'type': 'vector',
@@ -39,9 +22,9 @@ map.on('load', function() {
         'source-layer': 'Metropolitan_Statistical_Area-1sl8an', // This is usually the name of your tileset
         'layout': {},
         'paint': {
-            'fill-color': '#33FF57',  // Contrasting color
+            'fill-color': '#f7d797',  // Contrasting color
             'fill-opacity': 0.7,     // Medium-high opacity
-            'fill-outline-color': '#1D8C00'  // Thin or medium dark outline
+            'fill-outline-color': '#f7d797'  // Thin or medium dark outline
         }
     });
 
@@ -57,9 +40,9 @@ map.on('load', function() {
         'source-layer': 'Micropolitan_Statistical_Area-3zt3x6', // This is usually the name of your tileset
         'layout': {},
         'paint': {
-            'fill-color': '#338AFF',  // Another contrasting color
+            'fill-color': '#dbcdb2',  // Another contrasting color
             'fill-opacity': 0.6,     // Medium opacity
-            'fill-outline-color': '#1D4C8C'  // Medium dark outline
+            'fill-outline-color': '#dbcdb2'  // Medium dark outline
         }
     });
 
@@ -75,10 +58,30 @@ map.on('load', function() {
         'source-layer': 'Combined_Statistical_Area-8s87ox',
         'layout': {},
         'paint': {
-            'line-color': '#000000', 
+            'line-color': '#57544c', 
             'line-width': 2,  
         }
     }); 
+    map.addSource('Place', {
+        'type': 'vector',
+        'url': 'mapbox://nmgis.59eos659'
+    });
+
+    map.addLayer({
+        'id': 'Place_Layer',
+        'type': 'fill', // Can be 'fill', 'line', 'circle', 'symbol', etc. based on your data
+        'source': 'Place',
+        'source-layer': 'Incorporated_Place_Related-1qqx0j', // This is usually the name of your tileset
+        'layout': {},
+        'minzoom': 7,  // The layer will become visible starting at this zoom level
+        'maxzoom': 22, // The layer will become invisible after this zoom level
+        'paint': {
+            'fill-color': '#8c8c8c',  // Bright color
+            'fill-opacity': 0.7,     // High opacity
+            'fill-outline-color': '#8c8c8c'  // Thin dark outline
+        }
+    });
+    
 });
 
 // Add click event to the reset button
